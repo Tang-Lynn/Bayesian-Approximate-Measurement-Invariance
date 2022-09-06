9d991ec9a45a6ca0de4f02b489d06d6dd2ad61bc
Dandan Tang <71375838+Tang-Lynn@users.noreply.github.com>
2022-09-05 23:27:47 -0400

    Add files via upload

---
+++ example/example1.R
@@ -0,0 +1,28 @@
+library(bain)
+library(lavaan)
+read.csv(example_data1,header = T)
+# Italians
+Italians <- example_data1[1:313,]
+# Colombians
+Colombians <- example_data1[314:628,]
+psych::describe(Italians)
+psych::describe(Colombians)
+# CFA model
+model <- 'A  =~ Item1 + Item2 + Item3 + Item4 + Item5 + Item6 + Item7 + Item8 +
+                Item9 + Item10 + Item11 + Item12 + Item13 + Item14 + Item16 + Item17'
+# computing the model
+x <- cfa(model, data = example_data1, std.lv = TRUE, group = "COUNTRY")
+# fit indexes
+fit<- fitmeasures(lavout,c("chisq", "df",
+                             "pvalue", "cfi", "rmsea","srmr","BIC"),output = "matrix")
+fit
+# estimated parameters
+summary(x)
+
+# testing metric invariance
+set.seed(2020)
+BF_metric <- bmi(x,type = "metric",tolerance = .2,minBF = 3,fraction = 2)
+
+# testing Scalar invariance
+set.seed(2020)
+BF_scalar <- bmi(x,type = "scalar",tolerance = .2,minBF = 3,fraction = 2)
