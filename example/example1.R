c3e64814dc39016ba0f79f64a132beb4422906c4
Dandan Tang <71375838+Tang-Lynn@users.noreply.github.com>
2023-09-27 21:34:02 -0400

    Update example1.R

--- example/example1.R
+++ example/example1.R
@@ -9,7 +9,7 @@ psych::describe(Italians)
 psych::describe(Colombians)
 # CFA model
 model <- 'A  =~ Item1 + Item2 + Item3 + Item4 + Item5 + Item6 + Item7 + Item8 +
-                Item9 + Item10 + Item11 + Item12 + Item13 + Item14 + Item16 + Item17'
+                Item9 + Item10 + Item11 + Item12 + Item13 + Item14 + Item16'
 # computing the model
 x <- cfa(model, data = example_data1, std.lv = TRUE, group = "COUNTRY")
 # fit indexes
